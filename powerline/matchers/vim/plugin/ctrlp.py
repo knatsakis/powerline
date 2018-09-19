335157561d45c4ed881a05a291023473b5adc77a
Konstantinos Natsakis <k.natsakis@xe.gr>
2018-09-19 23:42:18 +0300

    ctrlp statusline - import os before vim

--- powerline/matchers/vim/plugin/ctrlp.py
+++ powerline/matchers/vim/plugin/ctrlp.py
@@ -1,12 +1,12 @@
 # vim:fileencoding=utf-8:noet
 from __future__ import (unicode_literals, division, absolute_import, print_function)
 
+import os
+
 try:
     import vim
 except ImportError:
     vim = object()
-
-import os
 
 from powerline.bindings.vim import buffer_name
 
