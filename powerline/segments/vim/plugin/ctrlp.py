9226b5fc74f0eec98b7c0c45cfc87443db3841de
Konstantinos Natsakis <k.natsakis@xe.gr>
2018-09-21 01:11:48 +0300

    ctrlp statusline - fix indentation

--- powerline/segments/vim/plugin/ctrlp.py
+++ powerline/segments/vim/plugin/ctrlp.py
@@ -11,70 +11,70 @@ from powerline.bindings.vim import vim_getvar
 
 
 def marked(pl):
-    '''Returns boolean indicating whether anything is marked or not.
+	'''Returns boolean indicating whether anything is marked or not.
 
-    Highlight groups used: ``ctrlp:marked``.
-    '''
-    status = vim_getvar('_powerline_ctrlp_status')
-    if 'prog' in status:
-        return []
-    return None if status['marked'] == ' <->' else [{
-        'highlight_groups': ['ctrlp:marked'],
-        'contents': status['marked'].strip()
-    }]
+	Highlight groups used: ``ctrlp:marked``.
+	'''
+	status = vim_getvar('_powerline_ctrlp_status')
+	if 'prog' in status:
+		return []
+	return None if status['marked'] == ' <->' else [{
+		'highlight_groups': ['ctrlp:marked'],
+		'contents': status['marked'].strip()
+	}]
 
 
 def mode(pl):
-    '''Returns current mode.
+	'''Returns current mode.
 
-    Highlight groups used: ``ctrlp:status``.
-    '''
-    status = vim_getvar('_powerline_ctrlp_status')
-    if 'prog' in status:
-        return []
-    return [{
-        'highlight_groups': ['ctrlp:status'],
-        'contents': ' ' + status['item'] + ' '
-    }]
+	Highlight groups used: ``ctrlp:status``.
+	'''
+	status = vim_getvar('_powerline_ctrlp_status')
+	if 'prog' in status:
+		return []
+	return [{
+		'highlight_groups': ['ctrlp:status'],
+		'contents': ' ' + status['item'] + ' '
+	}]
 
 
 def mode_prev(pl):
-    '''Returns previous mode.
+	'''Returns previous mode.
 
-    Highlight groups used: ``ctrlp:status_other``.
-    '''
-    status = vim_getvar('_powerline_ctrlp_status')
-    if 'prog' in status:
-        return []
-    return [{
-        'highlight_groups': ['ctrlp:status_other'],
-        'contents': status['prev'] + ' '
-    }]
+	Highlight groups used: ``ctrlp:status_other``.
+	'''
+	status = vim_getvar('_powerline_ctrlp_status')
+	if 'prog' in status:
+		return []
+	return [{
+		'highlight_groups': ['ctrlp:status_other'],
+		'contents': status['prev'] + ' '
+	}]
 
 
 def mode_next(pl):
-    '''Returns next mode.
+	'''Returns next mode.
 
-    Highlight groups used: ``ctrlp:status_other``.
-    '''
-    status = vim_getvar('_powerline_ctrlp_status')
-    if 'prog' in status:
-        return []
-    return [{
-        'highlight_groups': ['ctrlp:status_other'],
-        'contents': ' ' + status['next'] + ' '
-    }]
+	Highlight groups used: ``ctrlp:status_other``.
+	'''
+	status = vim_getvar('_powerline_ctrlp_status')
+	if 'prog' in status:
+		return []
+	return [{
+		'highlight_groups': ['ctrlp:status_other'],
+		'contents': ' ' + status['next'] + ' '
+	}]
 
 
 def prog(pl):
-    '''Returns progress status.
+	'''Returns progress status.
 
-    Highlight groups used: ``ctrlp:prog``.
-    '''
-    status = vim_getvar('_powerline_ctrlp_status')
-    if 'prog' not in status:
-        return []
-    return [{
-        'highlight_groups': ['ctrlp:prog'],
-        'contents': status['prog']
-    }]
+	Highlight groups used: ``ctrlp:prog``.
+	'''
+	status = vim_getvar('_powerline_ctrlp_status')
+	if 'prog' not in status:
+		return []
+	return [{
+		'highlight_groups': ['ctrlp:prog'],
+		'contents': status['prog']
+	}]
