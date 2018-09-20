61632aaca9397d6d5038561101e6beababd54a49
Konstantinos Natsakis <k.natsakis@xe.gr>
2018-09-21 00:58:16 +0300

    ctrlp statusline - fix indentation

--- powerline/matchers/vim/plugin/ctrlp.py
+++ powerline/matchers/vim/plugin/ctrlp.py
@@ -22,24 +22,24 @@ def ctrlp(matcher_info):
 	vim.command(
 		'''
 		function! Ctrlp_status_main(focus, byfname, regex, prev, item, next, marked)
-		let g:_powerline_ctrlp_status = {\
-		'focus': a:focus,\
-		'byfname': a:byfname,\
-		'regex': a:regex,\
-		'prev': a:prev,\
-		'item': a:item,\
-		'next': a:next,\
-		'marked': a:marked,\
-	}
-		return ''
+			let g:_powerline_ctrlp_status = {\
+			'focus': a:focus,\
+			'byfname': a:byfname,\
+			'regex': a:regex,\
+			'prev': a:prev,\
+			'item': a:item,\
+			'next': a:next,\
+			'marked': a:marked,\
+			}
+			return ''
 		endfunction
 		'''
 	)
 	vim.command(
 		'''
 		function! Ctrlp_status_prog(str)
-		let g:_powerline_ctrlp_status['prog'] = a:str
-		return ''
+			let g:_powerline_ctrlp_status['prog'] = a:str
+			return ''
 		endfunction
 		'''
 	)
